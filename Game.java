92d03d956a739d9782dd794fd377fb4bf8029ef4
Orsolya Lévai <solya0082@gmail.com>
2018-03-08 14:09:36 +0100

    Switched Array structure to HashMap

--- Game.java
+++ Game.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+import java.util.Iterator;
+import java.util.HashMap;
+
 /*
 Attribute:
     int public timePerFrame
@@ -22,9 +26,14 @@ public class Game {
         Game.initialize();
         while (true) {
             long startTime = System.currentTimeMillis();
-            for(int i = 0; i < Renderer.displayObjArray.length; i++) {
-                Renderer.displayObjArray[i].update();
+
+            Iterator it = Renderer.getIteratorForDisplayObjMap();
+            while(it.hasNext()) {
+                Map.Entry mapItem = (Map.Entry)it.next();
+                DisplayObj displayItem = (DisplayObj)mapItem.getValue();
+                displayItem.update();
             }
+
             Renderer.renderScreen();
             long endTime = System.currentTimeMillis();
             long deltaTime = endTime - startTime;
@@ -46,9 +55,13 @@ public class Game {
         DisplayObj testObject1 = new DisplayObj(5, 6, testSprites[0], testSprites[0], 10);
         DisplayObj testObject2 = new DisplayObj(50, 5, testSprites[1], testSprites[1], 10);
 
-        Renderer.displayObjArray = new DisplayObj[2];
-        Renderer.displayObjArray[0] = testObject1;
-        Renderer.displayObjArray[1] = testObject2;
+        //Renderer.displayObjArray = new DisplayObj[2];
+        //Renderer.displayObjArray[0] = testObject1;
+        //Renderer.displayObjArray[1] = testObject2;
+
+        Renderer.displayObjMap = new HashMap();
+        Renderer.displayObjMap.put("creature", testObject1);
+        Renderer.displayObjMap.put("heart", testObject2);
         Renderer.offsetX = 20;
         Renderer.offsetY = 0;
     }
