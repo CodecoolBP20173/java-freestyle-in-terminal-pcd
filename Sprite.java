cee3a01eda1f89494d9885c4eecfb3d1b2015d20
Andras Zibar <zibandris@gmail.com>
2018-03-08 15:33:52 +0100

    added two extra colors to the palette

--- Sprite.java
+++ Sprite.java
@@ -10,12 +10,14 @@ class Sprite {
 		Terminal t = new Terminal();
 		spritePalette.put('─', t.rgbColorBg(255, 255, 255));
 		spritePalette.put('G', t.rgbColorBg(127, 127, 127));
-		spritePalette.put('░', t.rgbColorBg(233, 227, 198));
+		spritePalette.put('░', t.rgbColorBg(239, 228, 177));
 		spritePalette.put('$', t.rgbColorBg(153, 217, 234));
 		spritePalette.put('M', t.rgbColorBg(185, 122, 87));
 		spritePalette.put('@', t.rgbColorBg(255, 174, 201));
 		spritePalette.put('B', t.rgbColorBg(112, 146, 190));
 		spritePalette.put('█', t.rgbColorBg(215, 76, 76));
+		spritePalette.put('W', t.rgbColorBg(60, 172, 13));
+		spritePalette.put('A', t.rgbColorBg(23, 27, 0));
 	}
 
 	public Sprite(String filepath, int numberOfFrames, int frameHeight) throws IOException {		
